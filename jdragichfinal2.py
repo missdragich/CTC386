37214993e6186954c4bf00f81ebbeaee1bcd6591
missdragich <missdragich@gmail.com>
2026-01-29 01:58:25 +0000

    final 2

---
+++ jdragichfinal2.py
@@ -0,0 +1,9 @@
+#JDragich final exam program 2
+def guess():
+    x = int(input(">"))
+    return x
+print("Enter a number between 1 and 10.")
+x = guess()
+for i in range(x):
+    print("Happy New Year!")
+
