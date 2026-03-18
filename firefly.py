75db38c272f89c085a21ce8a80951e626b1f5a94
missdragich <missdragich@gmail.com>
2026-03-18 23:14:36 +0000

    first commit

---
+++ firefly.py
@@ -0,0 +1,5 @@
+#jdragich
+name = input("Mornin'! What's your name?")
+print (name, "? That's shiny!")
+dest = input("Where are you headed?")
+print (dest, " ? That's pretty far out. We've got the best ship in the 'verse. She's a Firefly class.")
