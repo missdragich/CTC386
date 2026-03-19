3e714bc6db12cf756d3fbe4ffe7828e2be056cf1
missdragich <missdragich@gmail.com>
2026-03-19 00:33:05 +0000

    first commit

---
+++ midterm387.py
@@ -0,0 +1,7 @@
+#jdragich midterm 
+answer = int(input("Please enter a number between 1 and 10."))
+if (answer == 4):
+    print("Congrats! You are awesome1")
+else:
+    print("I'm sorry.", answer, " is not correct.")
+
