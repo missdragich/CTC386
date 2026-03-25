31342028f0ccf79bd81ee0064aad600d301daf7e
missdragich <missdragich@gmail.com>
2026-03-25 03:53:14 +0000

    first commit

---
+++ raccoon.py
@@ -0,0 +1,11 @@
+#jdragich raccon midterm extra credit
+say = input("What would you like the raccoon to say?")
+print("           ")
+print(say)
+print("           ")
+print("  ^----^    ")
+print("((-)..(-)) ")
+print("   (**)    ")
+print(" ()~**~()  ")
+print(" (_)**(_)  ")
+
